b9ee75be2d826f8262dfc7b7dac12c4dd29d47b3
amf6040 <57423076+amf6040@users.noreply.github.com>
2019-11-09 20:02:33 -0500

    final fix for initial ui

--- app/src/main/java/com/example/trippe/MainActivity.java
+++ app/src/main/java/com/example/trippe/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.trippe;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,11 +9,6 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.AdapterView.OnItemSelectedListener;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,19 +25,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-        // create dropdown for source currency selection
-        Spinner dropFromCurrency = (Spinner) findViewById(R.id.dropFromCurrency);
-
-        // arrayadaptor is used to add to the spinner dropdown
-        ArrayAdapter<CharSequence> currencyAdapter = ArrayAdapter
-                .createFromResource(this, R.array.currency_names,
-                        android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        //currencyAdapter
-        //       .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //dropFromCurrency.setAdapter(currencyAdapter);
-
-        // Apply the adapter to the spinner
-        //currencyAdapter.setAutofillOptions(currencyAdapter);
     }
+
 }
