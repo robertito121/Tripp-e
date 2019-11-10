6369a790351b317f5dcbe5f4a43a7528d37653c3
amf6040 <57423076+amf6040@users.noreply.github.com>
2019-11-09 20:55:27 -0500

    added a graph
    added a 5 day history graph to the ui - we can delete if we dont want the extra work

--- app/src/main/java/com/example/trippe/ui/currency/CurrencyFragment.java
+++ app/src/main/java/com/example/trippe/ui/currency/CurrencyFragment.java
@@ -16,6 +16,9 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.trippe.R;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -75,6 +78,21 @@ public class CurrencyFragment extends Fragment {
 
                 // attach our adaptor to our TO dropdown and populate the options
                 dropToCurrency.setAdapter(currencyAdapter);
+
+                //-------------------------------------------
+                // Testing a graph of currency histories
+                //-------------------------------------------
+                GraphView currencyGraph = (GraphView) root.findViewById(R.id.currencyGraph);
+                currencyGraph.setTitle("5 Day History");
+                LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                        new DataPoint(0, 1),
+                        new DataPoint(1, 5),
+                        new DataPoint(2, 3),
+                        new DataPoint(3, 2),
+                        new DataPoint(4, 6)
+                });
+
+                currencyGraph.addSeries(series);
 
             }
         });
