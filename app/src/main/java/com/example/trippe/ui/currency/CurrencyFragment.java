8e8dc741ec9b8fe97c336569b0a52d1759d74f85
amf6040 <57423076+amf6040@users.noreply.github.com>
2019-11-12 13:10:02 -0500

    Added error checking of form inputs
    Basic error checking and prompts via toasts for the currency conversion UI elements to be handled in the onClick method

--- app/src/main/java/com/example/trippe/ui/currency/CurrencyFragment.java
+++ app/src/main/java/com/example/trippe/ui/currency/CurrencyFragment.java
@@ -1,13 +1,16 @@
 package com.example.trippe.ui.currency;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Layout;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.util.Log;
@@ -23,6 +26,7 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,11 +38,65 @@ public class CurrencyFragment extends Fragment implements OnClickListener {
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnConvert:
-                TextView txtResult = (TextView) view.findViewById(R.id.txtResult); // notice the inflated view and not v here
-                txtResult.setText("Clicked");
-                break;
+        if (v.getId() == R.id.btnConvert) {
+            // get our UI elements to pull data
+            Spinner dropFromCurrency = (Spinner) view.findViewById(R.id.dropFromCurrency); // not sure why this cant resolve now, it works at runtime
+            Spinner dropToCurrency = (Spinner) view.findViewById(R.id.dropToCurrency); // same here
+            EditText txtFromAmount = (EditText) view.findViewById(R.id.txtFromAmount); // get our source currency amount
+            TextView txtResult = (TextView) view.findViewById(R.id.txtResult); // notice the inflated view and not v
+
+            // set our containers to hold ui element data
+            String fromCurrency;
+            String toCurrency;
+            Float fromAmount;
+
+            int red = Color.rgb(252, 159, 159);
+
+            try {
+                // retrieve the data from the ui elements
+                fromCurrency = dropFromCurrency.getSelectedItem().toString();
+                toCurrency = dropToCurrency.getSelectedItem().toString();
+                fromAmount = Float.valueOf(txtFromAmount.getText().toString());
+                Log.w("SHIT", "'" + toCurrency.length() + "'");
+                // Do our checks to make sure we have valid inputs before accessing the web API or database
+                if (fromAmount <= 0) { // make sure we got a positive amount of money to start
+                    Toast toast = Toast.makeText(getContext(), "Please enter a positive amount of money greater than zero", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                    txtFromAmount.setBackgroundColor(red);
+                    txtResult.setText("");
+                } else if (fromCurrency.length() == 0) { // make sure they selected a source currency
+                    txtFromAmount.setBackgroundColor(Color.WHITE); // reset our text color in case it was changed due to errors
+                    Toast toast = Toast.makeText(getContext(), "Please select a source currency", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                    txtResult.setText("");
+                } else if (toCurrency.length() == 0) { // make sure they selected a destination currency
+                    txtFromAmount.setBackgroundColor(Color.WHITE); // reset our text color in case it was changed due to errors
+                    Toast toast = Toast.makeText(getContext(), "Please select a currency to convert to", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                    txtResult.setText("");
+                } else if (fromCurrency == toCurrency) { // make sure the source and destination arent the same
+                    txtFromAmount.setBackgroundColor(Color.WHITE); // reset our text color in case it was changed due to errors
+                    Toast toast = Toast.makeText(getContext(), "The Target Currency is the Source Currency!\n Please Select a Different Currency", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                    txtResult.setText("");
+                } else {
+                    //TODO WEB API REQUEST/DB QUERY
+                    txtFromAmount.setTextColor(Color.WHITE); // reset our text color in case it was changed due to errors
+                    txtResult.setText("Success!");
+                }
+            } catch (Exception e) {
+                Log.e("CurrencyFragment", e.toString(), e);
+                txtFromAmount.setText("");
+                txtFromAmount.setBackgroundColor(red);
+                Toast toast = Toast.makeText(getContext(), "Please enter a positive amount of money greater than zero", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+                txtResult.setText("");
+            }
         }
     }
 
