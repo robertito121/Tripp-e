ff97d44285aad5aaddbb7625330d1a90250c6a85
robertito121 <37167135+robertito121@users.noreply.github.com>
2019-11-12 20:38:20 -0500

    Adding Trip Actions interface to support Trips  and Calendar fragments

---
+++ app/src/main/java/com/example/trippe/ui/trips/TripActions.java
@@ -0,0 +1,14 @@
+package com.example.trippe.ui.trips;
+
+public interface TripActions {
+
+    public void addTrip();
+
+    public boolean deleteExistingTrip();
+
+    public boolean modifyTrip();
+
+    public boolean completeTrip();
+
+
+}
